5b30d2d2d8c29b942894a98c76eaf936a6635d98
soyoung1452 <wls1452@naver.com>
2024-04-22 18:16:34 +0900

    del DyGlib

--- lib/DyGLib/preprocess_data/preprocess_all_data.py
+++
@@ -1,5 +0,0 @@
-import os
-
-for name in ['wikipedia', 'reddit', 'mooc', 'lastfm', 'myket', 'enron', 'SocialEvo',
-             'uci', 'Flights', 'CanParl', 'USLegis', 'UNtrade', 'UNvote', 'Contacts']:
-    os.system(f'python preprocess_data.py  --dataset_name {name}')
